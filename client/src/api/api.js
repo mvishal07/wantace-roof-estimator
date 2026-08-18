c66d1a0ab56fe56190d4acb81f81db147730a48c
Munagalashetty vishal <108283119+mvishal07@users.noreply.github.com>
2026-08-18 11:37:06 +0530

    Change API base URL to production endpoint

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://wantace-roof-estimator-f3qg.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
